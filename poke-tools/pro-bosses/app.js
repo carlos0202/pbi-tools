6081a011403d2dcbcd0ae5c89ea5c009d575bbc6
carlos.gonzalez <carlos.gonzalez@transperfect.com>
2024-12-11 01:25:49 +0100

    feat(app): ETA refresh
    pending: take load time to localstorage to recalculate eta better

--- poke-tools/pro-bosses/app.js
+++ poke-tools/pro-bosses/app.js
@@ -18,6 +18,7 @@ $(function () {
     var $weekStart = moment.utc('00:00:00', ['h:m a', 'H:m']).clone().weekday(0);
     var $globalBossesCounter = $('#btnGlobalBosses');
 
+    var regionListHtml = '';
     var bossIdMap = {};
     var regionIdMap = {};
     var containerId = $accordion.attr('id');
@@ -28,56 +29,52 @@ $(function () {
     var bossRegionMap = {};
     var regionFoughtMap = {};
     var regionBossesCountMap = {};
+    var clipText = '';
+    var loadedFromLocalStorage = false;
 
     console.log($weekStart);
 
-    var bossListJsonPromise = $.getJSON('assets/boss-list.json', function (result) {
-        bossesList = result;
-    });
-    promises.push(bossListJsonPromise);
+    reloadFromLocalStorageIfAvailable();
 
-    var bossTemplatePromise = $.get('templates/boss.mustache', function (bossTemplateStr) {
-        bossTemplate = bossTemplateStr;
-    });
-    promises.push(bossTemplatePromise);
+    if (!loadedFromLocalStorage) {
+        console.log('Localstorage data not available...');
 
-    var regionTemplatePromise = $.get('templates/region.mustache', function (regionTemplateStr) {
-        regionTemplate = regionTemplateStr;
-    });
-    promises.push(regionTemplatePromise);
-
-    $.when.apply($, promises).done(function () {
-        var regionListHtml = '';
-        $.each(bossesList, function (regionIndex, region) {
-            var bossListHtml = '';
-            var tableId = getBossId(region.region, regionIndex);
-            regionIdMap[region.region] = tableId;
-            regionBossesCountMap[tableId] = region.bossList.length;
-
-            $.each(region.bossList, function (bossIndex, boss) {
-                var bossId = getBossId(boss.name, boss.index);
-                bossIdMap[boss.name] = bossId;
-                bossRegionMap[bossId] = tableId;
-
-                var bossHtml = Mustache.render(bossTemplate, { ...boss, bossId });
-                bossListHtml += bossHtml;
-            });
-
-            var regionHtml = Mustache.render(regionTemplate, { ...region, regionIndex, containerId, bossesList: bossListHtml, tableId, totalBosses: region.bossList.length });
-            regionListHtml += regionHtml;
+        var bossListJsonPromise = $.getJSON('assets/boss-list.json', function (result) {
+            bossesList = result;
+            localStorage.setItem('bossesList', JSON.stringify(result));
         });
-        $accordion.html(regionListHtml);
+        promises.push(bossListJsonPromise);
+    
+        var bossTemplatePromise = $.get('templates/boss.mustache', function (bossTemplateStr) {
+            bossTemplate = bossTemplateStr;
+            localStorage.setItem('bossTemplate', bossTemplateStr);
+        });
+        promises.push(bossTemplatePromise);
+    
+        var regionTemplatePromise = $.get('templates/region.mustache', function (regionTemplateStr) {
+            regionTemplate = regionTemplateStr;
+            localStorage.setItem('regionTemplate', regionTemplateStr);
+        });
+        promises.push(regionTemplatePromise);
+    
+        $.when.apply($, promises).done(buildAccordion);
+    } else {
+        console.log('Loading from localstorage data...');
+        buildAccordion();
         clearBossProgress();
-        updateGlobalBossCounter();
-    });
+        processBossList(null);
+    }
 
     $btnLoadClipboard.on('click', function (evt) {
         evt.preventDefault();
 
         clearBossProgress();
 
-        navigator.clipboard.readText().then((clipText) => {
-            processBossList(clipText);
+        navigator.clipboard.readText().then((bossText) => {
+            processBossList(bossText);
+
+            clipText = bossText;
+            localStorage.setItem('bossProgress', bossText);
         });
     });
 
@@ -86,42 +83,10 @@ $(function () {
     });
 
     function processBossList(bossData) {
-        const headerText = '#Boss               Last fight  Duration';
-        const bossColumn = '#Boss';
-        const lastFoughtColumn = 'Last fight';
-        const cooldownColumn = 'Duration';
-        const bossNameStartIndex = headerText.indexOf(bossColumn);
-        const lastFoughtStartIndex = headerText.indexOf(lastFoughtColumn);
-        const cooldownStartIndex = headerText.indexOf(cooldownColumn);
+        var basicBossesInfo = getBasicBossInfo(bossData);
 
-        var basicBossesInfo = [];
-
-        if (bossData.indexOf(headerText) == -1) {
-            window.alert('text information to process is not a valid boss list text:\n\n' + bossData);
-
-            return;
-        }
-
-        const bossListLines = bossData.split('\n');
-
-        if (bossListLines.length < 2) {
-            window.alert('text information to process is an empty boss list text:\n\n' + bossData);
-
-            return;
-        }
-
-        for (const boss of bossListLines) {
-            if (boss.indexOf(bossColumn) == 0) continue;
-
-            basicBossesInfo.push({
-                bossName: boss.substring(bossNameStartIndex, lastFoughtStartIndex).trim(),
-                lastFought: boss.substring(lastFoughtStartIndex, cooldownStartIndex).trim(),
-                cooldown: boss.substring(cooldownStartIndex).trim()
-            });
-        }
-        console.log(basicBossesInfo);
-
-        console.log(bossIdMap);
+        console.log(`now processsing: `, bossData, basicBossesInfo);
+        if (!bossData && !basicBossesInfo) return;
 
         for (const boss of basicBossesInfo) {
             const bossId = bossIdMap[boss.bossName];
@@ -147,6 +112,14 @@ $(function () {
         }
 
         updateGlobalBossCounter();
+
+        setTimeout(function() {
+            console.log('executing...');
+
+            clearBossProgress();
+            processBossList(null);
+            
+        }, 1000);
     }
 
     function getBossId(bossName, bossIndex) {
@@ -180,13 +153,6 @@ $(function () {
             }
         });
 
-        // var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-        //     html: true,
-        //     trigger: 'focus',
-        //     content: function () {
-        //         return '<img src="'+$(this).data('bsContent') + '" />';
-        //     }
-        // });
         var $bossTeamModal = $('#bossTeamModal')
         $bossTeamModal.on('show.bs.modal', function (event) {
             // Button that triggered the modal
@@ -204,5 +170,279 @@ $(function () {
             $modalBodyInput.attr('src', teamImg);
         });
     }
+
+    function buildAccordion() {
+        
+        $.each(bossesList, function (regionIndex, region) {
+            var bossListHtml = '';
+            var tableId = getBossId(region.region, regionIndex);
+            regionIdMap[region.region] = tableId;
+            regionBossesCountMap[tableId] = region.bossList.length;
+
+            $.each(region.bossList, function (bossIndex, boss) {
+                var bossId = getBossId(boss.name, boss.index);
+                bossIdMap[boss.name] = bossId;
+                bossRegionMap[bossId] = tableId;
+
+                var bossHtml = Mustache.render(bossTemplate, { ...boss, bossId });
+                bossListHtml += bossHtml;
+            });
+
+            var regionHtml = Mustache.render(regionTemplate, { ...region, regionIndex, containerId, bossesList: bossListHtml, tableId, totalBosses: region.bossList.length });
+            regionListHtml += regionHtml;
+        });
+        $accordion.html(regionListHtml);
+        clearBossProgress();
+        updateGlobalBossCounter();
+    }
+
+    function reloadFromLocalStorageIfAvailable(){
+        if(storageAvailable('localStorage')){
+            var _bossesList = localStorage.getItem('bossesList');
+    
+            if (_bossesList != null)
+                bossesList = JSON.parse(_bossesList);
+            else {
+                loadedFromLocalStorage = false;
+
+                return;
+            }
+    
+            var _bossTemplate = localStorage.getItem('bossTemplate');
+    
+            if (_bossTemplate != null)
+                bossTemplate = _bossTemplate;
+            else {
+                loadedFromLocalStorage = false;
+
+                return;
+            }
+    
+            var _regionTemplate = localStorage.getItem('regionTemplate');
+    
+            if (_regionTemplate != null)
+                regionTemplate = _regionTemplate;
+            else {
+                loadedFromLocalStorage = false;
+
+                return;
+            }
+
+            loadedFromLocalStorage = true;
+        }
+    }
+
+    function refreshBossInfoCooldown(basicBossesInfo){
+        return basicBossesInfo.map(x => {
+            var hasCooldown = x.cooldown.indexOf('Ready') == -1;
+
+            console.log(hasCooldown, x);
+            var timeInfo, timeSubstracted, timeFormatted;
+
+            if (hasCooldown) {
+                timeInfo = getTimeLeftInfo(x.cooldown);
+                timeSubstracted = substractSecond(timeInfo);
+                timeFormatted = formatDaysString(timeSubstracted);
+                
+                console.log(timeInfo, timeSubstracted, timeFormatted);
+            }
+
+            return hasCooldown
+                ? Object.assign(x, { cooldown: timeFormatted })
+                : x;
+        });
+    }
+
+    function getBasicBossInfo(bossData) {
+        var basicBossesInfo = [];
+        var basicUpdatedBossInfo = [];
+        var loadedFromCache = false;
+
+        var _basicBossesInfo = localStorage.getItem('basicBossesInfo');
+        console.log(_basicBossesInfo, bossData == null, _basicBossesInfo != null);
+        if (bossData == null && _basicBossesInfo != null){
+            basicBossesInfo = JSON.parse(_basicBossesInfo);
+            loadedFromCache = true;
+
+            basicUpdatedBossInfo = refreshBossInfoCooldown(basicBossesInfo);
+
+            localStorage.setItem('basicBossesInfo', JSON.stringify(basicUpdatedBossInfo));
+
+            return basicBossesInfo;
+        }
+
+        console.log(_basicBossesInfo, bossData == null, _basicBossesInfo != null, loadedFromCache);
+        if (bossData == null && !loadedFromCache) return [];
+
+        const headerText = '#Boss               Last fight  Duration';
+        const bossColumn = '#Boss';
+        const lastFoughtColumn = 'Last fight';
+        const cooldownColumn = 'Duration';
+        const bossNameStartIndex = headerText.indexOf(bossColumn);
+        const lastFoughtStartIndex = headerText.indexOf(lastFoughtColumn);
+        const cooldownStartIndex = headerText.indexOf(cooldownColumn);
+
+        if (bossData.indexOf(headerText) == -1) {
+            window.alert('text information to process is not a valid boss list text:\n\n' + bossData);
+
+            return;
+        }
+
+        const bossListLines = bossData.split('\n');
+
+        if (bossListLines.length < 2) {
+            window.alert('text information to process is an empty boss list text:\n\n' + bossData);
+
+            return;
+        }
+
+        for (const boss of bossListLines) {
+            if (boss.indexOf(bossColumn) == 0) continue;
+
+            basicBossesInfo.push({
+                bossName: boss.substring(bossNameStartIndex, lastFoughtStartIndex).trim(),
+                lastFought: boss.substring(lastFoughtStartIndex, cooldownStartIndex).trim(),
+                cooldown: boss.substring(cooldownStartIndex).trim()
+            });
+        }
+        console.log(basicBossesInfo);
+
+        console.log(bossIdMap);
+
+        basicUpdatedBossInfo = refreshBossInfoCooldown(basicBossesInfo);
+
+        localStorage.setItem('basicBossesInfo', JSON.stringify(basicUpdatedBossInfo));
+
+        return basicBossesInfo;
+    }
 });
 
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      e.name === "QuotaExceededError" &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+// To use when I figure out how to get the exact date+time the boss was fought.
+function getTimeLeftInfo(timeStr) {
+    if (timeStr.toLowerCase().trim() == 'ready') return null;
+
+    var daysLeft = 0;
+    var hoursLeft;
+    var minutesLeft;
+    var secondsLeft;
+
+    var dayHours = timeStr.split(',');
+
+    if (dayHours.length  > 1){
+        daysLeft = parseInt(dayHours[0].trim().split(' ')[0]);
+        hourComponent = dayHours[1].split(':');
+        hoursLeft = parseInt(hourComponent[0]);
+        minutesLeft = parseInt(hourComponent[1]);
+        secondsLeft = parseInt(hourComponent[2]);
+    } else { 
+        var hourComponent = dayHours[0].trim().split(':');
+        hoursLeft = parseInt(hourComponent[0]);
+        minutesLeft = parseInt(hourComponent[1]);
+        secondsLeft = parseInt(hourComponent[2]);
+    }
+
+    var timeLeft = { 
+        days: daysLeft, 
+        hours: hoursLeft,
+        minutes: minutesLeft, 
+        seconds: secondsLeft 
+    };
+
+    return timeLeft;
+}
+
+function formatDaysString(daysFull){
+    console.log(daysFull, daysFull == null, !daysFull);
+
+    if (daysFull == null) return 'Ready';
+
+    //Get Days
+    const days = daysFull.days;
+    const daysFormatted = days ? `${days} days , ` : ''; // if no full days then do not display it at all
+  
+    //Get Hours
+    const hours = daysFull.hours;
+    const hoursFormatted = `${hours < 10 ? '0': ''}${hours}:`;
+  
+    //Get Minutes
+    const minutes = daysFull.minutes;
+    const minutesFormatted = minutes === 0
+        ? '00:'
+        : minutes < 10
+            ? `0${minutes}`
+            : `${minutes}:`;
+
+    //Get Seconds
+    const seconds = daysFull.seconds;
+    const secondsFormatted = seconds == 0
+        ? '00'
+        : (seconds < 10) 
+            ? `0${seconds}`
+            : `${seconds}`;
+  
+    return (days === 0 && minutes === 0 && seconds === 0)
+        ? 'Ready' 
+        : [daysFormatted, hoursFormatted, minutesFormatted, secondsFormatted].join('');
+}
+
+function substractSecond(daysFull){
+    if (daysFull.days === 0 &&  daysFull.minutes === 0 && daysFull.seconds === 0) return null;
+
+    var source = moment().startOf('month').set({ hour: 0, minute: 0, second: 0 })
+        .set({ date: daysFull.days, hour: daysFull.hours, minute: daysFull.minutes, second: daysFull.seconds })
+        .subtract(1, 'seconds');
+    
+    return { 
+        days: source.date(), 
+        hours: source.hours(), 
+        minutes: source.minutes(), 
+        seconds: source.seconds() 
+    };
+}
+
+function durationAsString(start, end) {
+    const duration = moment.duration(moment(end).diff(moment(start)));
+  
+    //Get Days
+    const days = Math.floor(duration.asDays()); // .asDays returns float but we are interested in full days only
+    const daysFormatted = days ? `${days} days ,` : ''; // if no full days then do not display it at all
+  
+    //Get Hours
+    const hours = duration.hours();
+    const hoursFormatted = `${hours}:`;
+  
+    //Get Minutes
+    const minutes = duration.minutes();
+    const minutesFormatted = minutes == 0
+        ? '00:'
+        : `${minutes}:`;
+
+    //Get Seconds
+    const seconds = duration.seconds();
+    const secondsFormatted = (seconds < 10) 
+        ? `0${seconds}`
+        : `${seconds}`;
+  
+    return ((days ?? 0) == minutes == seconds == 0)
+        ? 'Ready' 
+        : [daysFormatted, hoursFormatted, minutesFormatted, secondsFormatted].join('');
+  }
